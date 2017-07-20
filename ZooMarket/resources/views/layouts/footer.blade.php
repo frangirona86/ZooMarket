5bb68b4f61159052b86e168e5eff9a2600545fd8
Unknown <gironafrancisco86@gmail.com>
2017-07-20 11:17:43 -0300

    Footer + Index Beta

--- ZooMarket/resources/views/layouts/footer.blade.php
+++ ZooMarket/resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
-<footer class="footer">
+<!-- <footer class="footer">
   <div class="footer-div-superior">
     <div class="footer-container-1">
-      </div>
+      </div> -->
     <!-- <div class="footer-container">
       <form class="" action="" method="post">
         <div class="">
@@ -12,7 +12,7 @@
 
       </form>
     </div> -->
-  </div>
+  <!-- </div>
 
   <div class="footer-div-medio">
     <div class="footer-container">
@@ -57,4 +57,54 @@
   </div>
 </footer>
 </body>
+</html> -->
+<div class="content">
+</div>
+    <footer id="myFooter">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h2 class="logo"><a href="#"><img src="{{asset('images/footer/prueba-logo.png') }}" alt="logoZooMaket"> </a></h2>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Comienza</h5>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Registrarse</a></li>
+                        <li><a href="#">Loguearse</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Nosotros</h5>
+                    <ul>
+                        <li><a href="#">Información sobre nosotros</a></li>
+                        <li><a href="#">Preguntas Frecuentes</a></li>
+                        <li><a href="#">Contactenos</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Soporte</h5>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Mesa de Ayuda</a></li>
+                        <li><a href="#">Foro</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <div class="social-networks">
+                        <a href="https://twitter.com/?lang=es" target="_blank" class="twitter"><img width ="35px" src="{{asset('images/footer/logo-twitter.png') }}"  alt=""></a>
+                        <a href="https://www.facebook.com/" target="_blank" class="facebook"><img width="35px" src="{{asset('images/footer/facebook-logo-button.png') }}"alt=""></i></a>
+                        <a href="https://www.instagram.com/?hl=es" target="_blank" class="google"><img width="35px" src="{{asset('images/footer/instagram-logo.png') }}" alt=""></a>
+                    </div>
+                    <button type="button" class="btn btn-default">Usuarios !</button>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2017 Copyright ZooMarket </p>
+        </div>
+    </footer>
+
+</body>
+
 </html>
--- ZooMarket/resources/views/welcome.blade.php
+++ ZooMarket/resources/views/welcome.blade.php
@@ -1,48 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="content">
-    <div class="body_home">
-      <div class="principalContenedorBanner">
-        <div class="w3-content w3-display-container" style="">
-          <img class="mySlides" src="{{asset('images/index/gatoBanner.jpg')}}" style="width:100%">
-          <img class="mySlides" src="{{asset('images/index/perroBanner.jpg')}}" style="width:100%; display: none;">
-          <img class="mySlides" src="{{asset('images/index/birdBanner.jpg')}}" style="width:100%; display: none;">
-          <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
-            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
-            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
-            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
-          </div>
-        </div>
-      </div>
+<header class="business-header" id="bannerIndex">
+    <!-- <div class="container"> -->
+        <div class="row">
 
-      <section class="body_products_grilla">
-        <article class="products_body col-md-3">
-          <img src="{{asset('/images/index/perro_portada_seccion.jpg')}}" alt="">
-          <div class="title.product">
-            <a href="#">Perros!</a>
-          </div>
-        </article>
-        <article class="col-md-3 products_body">
-          <img src="{{asset('/images/index/gato_portada_seccion.jpg')}}" alt="">
-          <div class="title.product">
-            <a href="#">Gatos!</a>
-          </div>
-        </article>
-        <article class="col-md-3 products_body">
-          <img src="{{asset('/images/index/cruza_portada.jpg')}}" alt="">
-          <div class="title.product">
-            <a href="#">Ofertas!</a>
-          </div>
-        </article>
-        <article class="col-md-3 products_body">
-          <img src="{{asset('/images/index/otras_mascotas_portada.jpg')}}" alt="">
-          <div class="title.product">
-            <a href="#">Otras mascotas!</a>
-          </div>
-        </article>
-      </section>
+                  <img class="mySlides" src="{{asset('images/index/gatoBanner.jpg')}}" style="width:100%;">
+                  <img class="mySlides" src="{{asset('images/index/perroBanner.jpg')}}" style="width:100%; display: none;">
+                  <img class="mySlides" src="{{asset('images/index/birdBanner.jpg')}}" style="width:100%; display: none;">
+                  <!-- <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+                    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+                    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+                    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+                  </div> -->
+
+            </div>
+        <!-- </div> -->
+  </header>
+
+<!-- Page Content -->
+<div class="container">
+
+
+    <!-- /.row -->
+
+    <hr>
+
+    <div class="row" >
+        <div class="col-sm-4">
+            <img class="img-circle img-responsive img-center" src="{{asset('/images/index/perro_portada_seccion.jpg')}}" alt="perroPortada">
+            <h2>Perros</h2>
+            <p>Aqui pondremos los que se puede encontrar sobre PERROS(Breve descripcion).</p>
+        </div>
+        <div class="col-sm-4">
+            <img class="img-circle img-responsive img-center" src="{{asset('/images/index/gato_portada_seccion.jpg')}}" alt="gatoPortada">
+            <h2>Gatos</h2>
+            <p>Aqui pondremos los que se puede encontrar sobre GATOS(Breve descripcion).</p>
+        </div>
+        <div class="col-sm-4">
+            <img class="img-circle img-responsive img-center" src="{{asset('/images/index/otras_mascotas_portada.jpg')}}" alt="otrasMascotas">
+            <h2>Otras Mascotas</h2>
+            <p>Aqui pondremos los que se puede encontrar sobre OTRAS MASCOTAS(Breve descripcion).</p>
+        </div>
     </div>
-  </div>
+
 
 @endsection
